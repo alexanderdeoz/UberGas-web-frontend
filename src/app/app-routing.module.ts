37013f224b03022a3074b8385b120378f619472e
alexanderdeoz <sad.chinchin@yavirac.edu.ec>
2021-08-03 22:11:06 -0500

    'v1'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',//inicio de uber Gas
+    component: MainComponent
+  },
+  {
+    path: 'ubergas',
+    component: MainComponent
+  },
+  {
+    path: 'not-found',//Componente no encontrado 
+    component: NotFoundComponent
+  },
+  {
+    path: '**', //componente no encontrado 
+    redirectTo: '/not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { UnderMaintenanceComponent } from './under-maintenance/under-maintenance.component';
+import { MainComponent } from './main/main.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { DistribuidorComponent } from './distribuidor/distribuidor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    AccessDeniedComponent,
+    UnderMaintenanceComponent,
+    MainComponent,
+    UsuarioComponent,
+    DistribuidorComponent
   ],
   imports: [
     BrowserModule,
